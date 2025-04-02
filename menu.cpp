e393cd4d39da6e54ad65b892f3bf2eb208f1c3ce
Thomas <Bader.Thomas96@hotmail.de>
2025-04-02 10:16:40 +0200

    test update background menü

--- menu.cpp
+++ menu.cpp
@@ -9,7 +9,14 @@
 MainWindow::MainWindow(QWidget *parent) : QWidget(parent), gameWindow(nullptr) {
     setWindowTitle("Game Menu");
     setFixedSize(1024, 512);
+
     //Button
+    QPixmap background(":/Grafik/Background.png");
+
+    this->setStyleSheet("QWidget {background-image: url(:/Grafik/Background.png);"
+                        "background-position: center;"
+                        "background-repeat: no-repeat;"
+                        "background-size: cover; }");
 
     QVBoxLayout *layout = new QVBoxLayout(this);
 
@@ -21,6 +28,11 @@ MainWindow::MainWindow(QWidget *parent) : QWidget(parent), gameWindow(nullptr) {
 
     layout->addWidget(startGameButton,0, Qt::AlignHCenter);
     layout->addWidget(highscoreButton,0, Qt::AlignHCenter);
+    startGameButton->setFixedSize(400, 100);
+    highscoreButton->setFixedSize(400, 100);
+
+    layout->addWidget(startGameButton, 0, Qt::AlignHCenter);
+    layout->addWidget(highscoreButton, 0, Qt::AlignHCenter);
 
     connect(startGameButton, &QPushButton::clicked, this, &MainWindow::startGame);
     connect(highscoreButton, &QPushButton::clicked, this, &MainWindow::showHighscore);
@@ -40,3 +52,20 @@ void MainWindow::startGame() {
 void MainWindow::showHighscore() {
     QMessageBox::information(this, "Highscore", "Displaying high scores...");
 }
+/*
+ * Hinzufügen des Hintergrundes.
+
+/*void MainWindow::paintEvent(QPaintEvent *event)
+{
+    QPainter Farbe(this); // QPainter ist von Qt selbst
+
+    // Zeichne das Viereck (Spieler)und die jeweilige Farbe
+    Farbe.setBrush(Qt::green);
+    Farbe.drawRect(viereckX, viereckY, viereckB, viereckH);
+
+    // Zeichne die Hindernisse und die jeweilige Farbe
+    Farbe.setBrush(Qt::red);
+    for (const Obstacle &obstacle : obstacles) {
+        Farbe.drawRect(obstacle.getRect());
+    }
+*/
